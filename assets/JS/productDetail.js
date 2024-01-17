61a169aa31968e6a09c52339675b102f22532f56
OrlandoConci <orlandomarceloconci@gmail.com>
2024-01-17 12:42:06 -0300

    Details

--- assets/JS/productDetail.js
+++ assets/JS/productDetail.js
@@ -3,29 +3,37 @@ const $productOnly = document.getElementById("productOnly")
 const queryParams = new URLSearchParams(location.search)
 const id = queryParams.get("id")
 
-fetch("https://moviestack.onrender.com/api/petshop")
-    .then( respuesta => respuesta.json())
-    .then( articuloApi => {
-        const articulo = articuloApi.find( articulo => articulo.id == id)
-        let disp = ""
-        if (articulo.disponibles <= 5) {
-            disp = "Últimas disponibles!" 
-        } 
+const urlApi = "https://moviestack.onrender.com/api/petshop";
 
-        $productOnly.innerHTML = `
-        <article>
-            <img src="${articulo.imagen} alt="Imágen ilustratica de un/a ">
-            <h1>${articulo.producto}</h1>
-            <div>
-                <p>${articulo.precio.toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}</p>
-                <div>
-                    <p>Stock: ${articulo.disponibles}</p>
-                    <p>${disp}</p>
-                </div>
-            </div>
-            <p>${articulo.descripcion}
-        </article>`
+export let articulos = [];
 
+await fetch(urlApi)
+.then(response => response.json())
+.then(data => {
+    articulos = data;
+    return articulos; 
+})
+.catch(err => console.log(err));
 
-    })
-    .catch(error => error)
+ console.log(articulos)
+
+const articulo = articulos.find( articulo => articulo.id == id)
+let disp = ""
+if (articulo.disponibles <= 5) {
+    disp = "Últimas disponibles!" 
+} 
+
+$productOnly.innerHTML = `
+<article>
+    <img src="${articulo.imagen} alt="Imágen ilustratica de un/a ">
+    <h1>${articulo.producto}</h1>
+    <div>
+        <p>${articulo.precio.toLocaleString( 'en-US', { style:'currency', currency:'USD' } )}</p>
+        <div>
+            <p>Stock: ${articulo.disponibles}</p>
+            <p>${disp}</p>
+        </div>
+    </div>
+    <p>${articulo.descripcion}
+</article>`
+
